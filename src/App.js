77eb87558f973b07a1b76509de2620a57d3eb8f6
RKEndToEnd <rkendtoend@gmail.com>
2022-05-08 00:07:26 +0200

    user register function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './scss/style.scss'
+import axios from 'axios'
 
 const loading = (
   <div className="pt-3 text-center">
@@ -17,6 +18,10 @@ const Login = React.lazy(() => import('./views/pages/login/Login'))
 const Register = React.lazy(() => import('./views/pages/register/Register'))
 const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+/*axios.defaults.baseURL = 'http://127.0.0.1:8000'*/
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.post['Accept'] = 'application/json'
+axios.defaults.withCredentials = true
 
 class App extends Component {
   render() {
--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   CButton,
   CCard,
@@ -14,19 +14,54 @@ import {
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
 import Home from '../home/Home'
+import axios from 'axios'
+import swal from 'sweetalert'
+import { useNavigate } from 'react-router-dom'
 
-const Register = () => {
+function Register() {
+  const [registerInput, setRegister] = useState({
+    name: '',
+    email: '',
+    password: '',
+    error_list: [],
+  })
+  const navigate = useNavigate()
+  const fetchInput = (e) => {
+    e.persist()
+    setRegister({ ...registerInput, [e.target.name]: e.target.value })
+  }
+  const Submit = (e) => {
+    e.preventDefault()
+    const data = {
+      name: registerInput.name,
+      email: registerInput.email,
+      password: registerInput.password,
+    }
+    axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then((response) => {
+      axios.post('http://127.0.0.1:8000/api/register', data).then((res) => {
+        if (res.data.status === 200) {
+          localStorage.setItem('auth_token', res.data.token)
+          localStorage.setItem('auth_name', res.data.username)
+          swal('Konto zostało utworzone', res.data.message, 'success')
+          navigate('/')
+        } else {
+          setRegister({ ...registerInput, error_list: res.data.val_errors })
+        }
+      })
+    })
+  }
   return (
     <>
       <Home />
-      <div className="bg-dark min-vh-100 d-flex flex-row align-items-center">
+      <div className="bg-dark min-vh-100 d-flex flex-row align-items-center small">
         <CContainer>
           <CRow className="justify-content-center">
             <CCol className="d-flex justify-content-center align-items-center">
               <CCard className="mx-12">
                 <CCardBody className="col-md-auto">
-                  <CForm>
+                  <CForm onSubmit={Submit}>
                     <h3 className="text-center">Rejestracja</h3>
+                    <span className="text-danger small">{registerInput.error_list.name}</span>
                     <CInputGroup className="mb-3">
                       <CInputGroupText>
                         <CIcon icon={cilUser} />
@@ -35,20 +70,26 @@ const Register = () => {
                         type="text"
                         floatingLabel="Nazwa użytkownika"
                         placeholder="Nazwa użytkownika"
-                        autoComplete="username"
+                        name="name"
                         style={{ height: '55px' }}
+                        onChange={fetchInput}
+                        value={registerInput.name}
                       />
                     </CInputGroup>
+                    <span className="text-danger small">{registerInput.error_list.email}</span>
                     <CInputGroup className="mb-3">
                       <CInputGroupText>@</CInputGroupText>
                       <CFormInput
                         type="email"
                         floatingLabel="Email"
                         placeholder="Email"
-                        autoComplete="email"
+                        name="email"
                         style={{ height: '55px' }}
+                        onChange={fetchInput}
+                        value={registerInput.email}
                       />
                     </CInputGroup>
+                    <span className="text-danger small">{registerInput.error_list.password}</span>
                     <CInputGroup className="mb-3">
                       <CInputGroupText>
                         <CIcon icon={cilLockLocked} />
@@ -57,11 +98,13 @@ const Register = () => {
                         type="password"
                         floatingLabel="Hasło"
                         placeholder="Hasło"
-                        autoComplete="new-password"
+                        name="password"
                         style={{ height: '55px' }}
+                        onChange={fetchInput}
+                        value={registerInput.password}
                       />
                     </CInputGroup>
-                    <CInputGroup className="mb-4">
+                    {/*<CInputGroup className="mb-4">
                       <CInputGroupText>
                         <CIcon icon={cilLockLocked} />
                       </CInputGroupText>
@@ -69,12 +112,14 @@ const Register = () => {
                         type="password"
                         floatingLabel="Powtórz hasło"
                         placeholder="Powtórz Hasło"
-                        autoComplete="new-password"
+                        name="confirmPassword"
                         style={{ height: '55px' }}
+                        onChange={fetchInput}
+                        value={registerInput.confirmPassword}
                       />
-                    </CInputGroup>
+                    </CInputGroup>*/}
                     <div className="d-grid">
-                      <CButton color="outline-success" className="btn-sm">
+                      <CButton type="submit" color="outline-success" className="btn-sm">
                         Utwórz konto
                       </CButton>
                     </div>
